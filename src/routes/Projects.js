cb10bdc8ecc9582b49e4a16b48bb609c3b6920a5
kurapatianurag1 <kurapatianurag1@gmail.com>
2020-11-22 11:56:00 -0500

    UpdatedCode

--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -16,7 +16,7 @@ function Projects() {
       <ProjectDetails
         companyname="CITI Bank Web Application , INDIA"
         dateDetails="Jan 2018-July 2019"
-        technicalrelated="Technical Skills Used: ReactJS, NodeJS, Redux, MongoDB, MySQL, Rest API’s,Postman, Html, CSS, Bootstrap"
+        
         BulletPoints1="It is a Banking Application designed for Customers to Perform Account Opening, Transfers, Open New Deposit, UPI, Bill Payments etc."
         BulletPoints2="Development work included building, implementing the UI/UX based on the design and achieving the banking functionalities."
         BulletPoints3="Worked in Dierent Browsers to achieve the Requirement."
@@ -24,7 +24,7 @@ function Projects() {
       <ProjectDetails
         companyname="Equitas Bank AOS Application, INDIA"
         dateDetails="Jan 2017-Mar 2018"
-        technicalrelated="Technical Skills Used: ReactJS, NodeJS, Redux, MongoDB, MySQL, Rest API’s,Postman, Html, CSS, Bootstrap"
+        
         BulletPoints1="Account Opening Application serves for Account Opening Purpose where Bank Users can open an Account for Customer. "
         BulletPoints2=" Delivered the Application with the best Quality driven and with best User Experience for bank users."
         BulletPoints3=" Worked on Login Screen, Navigations, OTP Timer, MPIN, API Calls to External Systems."
